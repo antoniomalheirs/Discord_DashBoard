4abf00d7d451e07a7b89ed1b08ba80ba2f17f65d
Antônio Malheiros <antoniomalhe@gmail.com>
2024-02-07 00:38:46 -0300

    Configurando Tailiwind to work proberly

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.css",
+    "./src/public/css/*.css",
+    "./src/public/layouts/*.ejs",
+    "./src/views/*.ejs"
+     // Corrected path and added wildcard
   ],
   theme: {
     extend: {
       colors: {
+        // You can extend colors here if needed
       },
     },
   },
   plugins: [],
 }
-
